388f7c6a82655f8b4c197a52cc1f7bfe66dea4e1
jon <joncastas@gmail.com>
2025-02-03 12:34:41 +0100

    pull added on second brain load

--- .config/i3/scripts/tmux_secondBrain.sh
+++ .config/i3/scripts/tmux_secondBrain.sh
@@ -2,17 +2,14 @@
 
 path="$HOME/Documents/secondBrain/"
 SESSION_NAME="secondBrain"
-KITTY_CLASS="kitty"
 
-git -C "$path" pull
-
-# Check if you're already in the session
-if tmux ls | grep -q "$SESSION_NAME:.*attached"; then
-    # Detach from the session if attached
-    tmux detach -s "$SESSION_NAME"
+# Check if the session exists and is attached
+if tmux list-sessions | grep -q "$SESSION_NAME:.*attached"; then
+	# Detach from the session if attached
+	tmux detach -s "$SESSION_NAME"
 else
-    # Attach to the session (or create it if it doesn't exist)
-    kitty --class "$KITTY_CLASS" --title "$SESSION_NAME" tmux attach-session -t "$SESSION_NAME" || \
-    kitty --class "$KITTY_CLASS" --title "$SESSION_NAME" tmux new-session -s "$SESSION_NAME"
+	# Attach to the session
+	kitty -1 --title "$SESSION_NAME" tmux attach -t "$SESSION_NAME"
+	git -C "$path" pull
 fi
 
