3fe5d9fc876666b3edb7f3c2c01164e858136da0
jon <joncastas@gmail.com>
2025-03-12 16:11:46 +0100

    basic gitsigns config added

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -3,6 +3,10 @@ return {
   "lewis6991/gitsigns.nvim",
 	config =function ()
 		require("gitsigns").setup()
+
+		vim.keymap.set('n', "<leader>gp", ':Gitsigns preview_hunk<CR>', { desc = "Git preview hunk" })
+		vim.keymap.set('n', "<leader>gi", ':Gitsigns preview_hunk_inline<CR>', { desc = "Git preview hunk inline" })
+		vim.keymap.set('n', "<leader>gb", ':Gitsigns toggle_current_line_blame<CR>', { desc = "Git blame" })
 	end
 },
 }
