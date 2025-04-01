8b7bd21c61aa6d42051ad7e3297ce644cfb2e57f
jon <joncastas@gmail.com>
2025-04-01 11:43:51 +0200

    nvim:clipboard history for neoclip

--- .config/nvim/lua/plugins/clipboard-history.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'royanirudd/clipboard-history.nvim',
-    opts = {
-        max_history = 200  -- Optional: set max history (default 100)
-    },
-	config = function()
-		vim.keymap.set({ 'n', 'v', 'i' }, "<M-v>", "<Esc>:ClipboardHistory<CR>", { noremap = true, silent = true })
-	end
-}
