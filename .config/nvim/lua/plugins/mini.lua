ee7ac7918382da974ac202dd21b6f984b64b6c56
jon <joncastas@gmail.com>
2025-04-01 10:09:00 +0200

    nvim: mini plugins added

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -34,6 +34,45 @@ return {
 			}
 		end
 	}, {
+		'echasnovski/mini.ai',
+		config = function()
+			require("mini.ai").setup{
+				--{ ( `Table` "with" 'textobject' ``) }id as fields(), textobject specification as values.
+				-- Also use this to disable builtin textobjects. See |MiniAi.config|.
+				custom_textobjects = nil,
+
+				-- Module mappings. Use `''` (empty string) to disable one.
+				mappings = {
+					-- Main textobject prefixes
+					around = 'a',
+					inside = 'i',
+
+					-- Next/last variants
+					around_next = 'an',
+					inside_next = 'in',
+					around_last = 'al',
+					inside_last = 'il',
+
+					-- Move cursor to corresponding edge of `a` textobject
+					goto_left = 'g[',
+					goto_right = 'g]',
+				},
+
+				-- Number of lines within which textobject is searched
+				n_lines = 50,
+
+				-- How to search for object (first inside current line, then inside
+				-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+				-- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+				search_method = 'cover_or_next',
+
+				-- Whether to disable showing non-error feedback
+				-- This also affects (purely informational) helper messages shown after
+				-- idle time if user input is required.
+				silent = false,
+			}
+		end,
+	},{ 
 		'echasnovski/mini.files',
 		config = function()
 			local MiniFiles = require("mini.files")
@@ -51,6 +90,7 @@ return {
 				MiniFiles.reveal_cwd()
 			end, { desc = "Toggle into currently opened file" })
 		end,
+	}, {
+		'echasnovski/mini.notify'
 	},
-
 }
