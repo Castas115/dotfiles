bd66168e0ba9d9b065eb19c8cb7191cd3db24c61
jon <joncastas@gmail.com>
2025-04-27 13:49:01 +0200

    nvim: telescope cleaup

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,23 +1,23 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "[F]ind [K]ey maps" })
-			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
-			vim.keymap.set("n", "<leader>fd", function()
-				require("telescope.builtin").buffers(require("telescope.themes").get_ivy({
-					sort_mru = true,
-					sort_lastused = true,
-					initial_mode = "normal",
-					layout_config = {
-						preview_width = 0.6,
-					},
-				}))
-			end, { desc = "Open telescope buffers" })
-		end,
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
+			{
+				"<leader>fd",
+				function()
+					require("telescope.builtin").buffers(require("telescope.themes").get_ivy({
+						sort_mru = true,
+						sort_lastused = true,
+						initial_mode = "normal",
+						layout_config = {
+							preview_width = 0.6,
+						},
+					}))
+				end,
+				desc = "Open telescope buffers",
+			},
 			{
 				"<leader>ff",
 				function()
@@ -56,12 +56,11 @@ return {
 			},
 
 			{
-				"<leader>gs",
+				"<leader>fg",
 				function()
 					require("telescope.builtin").git_status(require("telescope.themes").get_ivy({
 						layout_config = {
-							-- Set preview width, 0.7 sets it to 70% of the window width
-							preview_width = 0.7,
+							preview_width = 0.75,
 							-- height = 0.2,
 						},
 						initial_mode = "normal", -- Start in normal mode
@@ -75,40 +74,19 @@ return {
 				"<cmd>e #<cr>",
 				desc = "Alternate buffer",
 			},
-
-			{
-				"<leader>tl",
-				"<cmd>TodoTelescope keywords=TODO<cr>",
-				desc = "[P]TODO list (Telescope)",
-			},
-
-			{
-				"<leader>ta",
-				"<cmd>TodoTelescope keywords=PERF,HACK,TODO,NOTE,FIX<cr>",
-				desc = "[P]TODO list ALL (Telescope)",
-			},
+			{ "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "[F]ind [K]ey maps" }},
+			{ "<leader>fs", require("telescope.builtin").lsp_document_symbols, { desc = "Find Symbols" }},
 		},
 		opts = function()
+			local actions = require("telescope.actions")
 			return {
 				defaults = {
-					-- Prevents cycle cycling looping through results when reach the end
-					-- The default value is "cycle"
 					scroll_strategy = "limit",
-					-- I'm adding this `find_command` based on this reddit discussion
-					-- https://www.reddit.com/r/neovim/comments/1egczrs/comment/lfsotjx/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-					-- However, it doesn't work, also tried without the setup function
-					require("telescope").setup({
-						pickers = {
-							find_files = {
-								find_command = { "rg", "--files", "--sortr=modified" },
-							},
+					pickers = {
+						find_files = {
+							find_command = { "rg", "--files", "--sortr=modified" },
 						},
-					}),
-					-- When I search for stuff in telescope, I want the path to be shown
-					-- first, this helps in files that are very deep in the tree and I
-					-- cannot see their name.
-					-- Also notice the "reverse_directories" option which will show the
-					-- closest dir right after the filename
+					},
 					path_display = {
 						filename_first = {
 							reverse_directories = true,
@@ -116,23 +94,14 @@ return {
 					},
 					mappings = {
 						n = {
-							-- I'm used to closing buffers with "d" from bufexplorer
-							["d"] = require("telescope.actions").delete_buffer,
-							-- I'm also used to quitting bufexplorer with q instead of escape
-							["q"] = require("telescope.actions").close,
-							["<esc>"] = require("telescope.actions").close,
+							["d"] = actions.delete_buffer,
+							["q"] = actions.close,
+							["<esc>"] = actions.close,
 						},
-						-- -- When in insert mode, I want to quit telescope when I press escape
-						-- -- instead of going to normal mode, to go to normal mode I can press
-						-- -- my regular `kj`
-						-- -- https://www.reddit.com/r/neovim/comments/1ghzlx4/comment/lv3jg0n/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-						-- i = {
-						--   ["<esc>"] = require("telescope.actions").close,
-						-- },
 					},
 				},
 			}
-		end
+		end,
 	},
 
 	-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/telescope-frecency.lua
@@ -141,7 +110,7 @@ return {
 	--[=====[
 https://github.com/nvim-telescope/telescope-frecency.nvim
 
-This plugins keeps a score of my recently access files through telescope, and 
+This plugins keeps a score of my recently access files through telescope, and
 shows the ones I se the most at the top of the list
 
 It requires telescope, so don't uninstall telescope
@@ -151,9 +120,9 @@ https://github.com/nvim-telescope/telescope-frecency.nvim/blob/master/doc/telesc
 
 You can delete entries from DB by this command. This command does not remove
 the file itself, only from DB.
-- delete the current opened file 
+- delete the current opened file
   - :FrecencyDelete
-- delete the supplied path 
+- delete the supplied path
   - :FrecencyDelete /full/path/to/the/file
 --]=====]
 
@@ -189,6 +158,5 @@ the file itself, only from DB.
 			})
 			require("telescope").load_extension("frecency")
 		end,
-	}
+	},
 }
-
