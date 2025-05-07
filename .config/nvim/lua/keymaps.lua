f0b2fcae89b712f9ea7815885ffd2a5a30b2f02e
jon <joncastas@gmail.com>
2025-05-07 16:27:55 +0200

    nvim: cleaner keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,50 +1,50 @@
+local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>q", ":q!<CR>", opts)
-vim.keymap.set("n", "<M-q>", ":qa!<CR>", opts)
-vim.keymap.set({ 'n', 'v' }, "<C-S-c>", '"+y', opts)
-vim.keymap.set({ 'n', 'v' }, "<C-c>", '"+y', opts)
+keymap.set("n", "<leader>q", ":q!<CR>", opts)
+keymap.set("n", "<M-q>", ":qa!<CR>", opts)
+keymap.set({ 'n', 'v' }, "<C-S-c>", '"+y', opts)
+keymap.set({ 'n', 'v' }, "<C-c>", '"+y', opts)
 
-vim.keymap.set({ 'n', 'v' }, '<C-s>', '<Esc>:w<CR>')
-vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a')
+keymap.set({ 'n', 'v' }, '<C-s>', '<Esc>:w<CR>')
+keymap.set('i', '<C-s>', '<Esc>:w<CR>a')
 
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+keymap.set('n', '<C-h>', '<C-w>h', opts)
+keymap.set('n', '<C-j>', '<C-w>j', opts)
+keymap.set('n', '<C-k>', '<C-w>k', opts)
+keymap.set('n', '<C-l>', '<C-w>l', opts)
 
 vim.api.nvim_set_keymap('n', '<A-h>', ':vertical resize -2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-j>', ':resize -2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-k>', ':resize +2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-l>', ':vertical resize +2<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', 'tq', '<Esc>:bdelete<CR>')
-vim.keymap.set('n', 'tk', '<Esc>:bnext<CR>')
-vim.keymap.set('n', 'tj', '<Esc>:bprev<CR>')
+keymap.set('n', 'tq', '<Esc>:bdelete<CR>')
+keymap.set('n', 'tk', '<Esc>:bnext<CR>')
+keymap.set('n', 'tj', '<Esc>:bprev<CR>')
 
-vim.keymap.set('i', "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+keymap.set('i', "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 
-vim.keymap.set({ 'n', 'v', 'i', 'c' }, "<PageUp>", "<C-u>zz", { desc = "Scroll up half a page" })
-vim.keymap.set({ 'n', 'v', 'i', 'c' }, "<PageDown>", "<C-d>zz", { desc = "Scroll down half a page" })
+keymap.set({ 'n', 'v', 'i', 'c' }, "<PageUp>", "<C-u>zz", { desc = "Scroll up half a page" })
+keymap.set({ 'n', 'v', 'i', 'c' }, "<PageDown>", "<C-d>zz", { desc = "Scroll down half a page" })
 
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
+keymap.set("v", "<", "<gv", opts)
+keymap.set("v", ">", ">gv", opts)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv",
+keymap.set("v", "J", ":m '>+1<CR>gv=gv",
 	{ desc = "moves lines down in visual selection", noremap = true, silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv",
+keymap.set("v", "K", ":m '<-2<CR>gv=gv",
 	{ desc = "moves lines up in visual selection", noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader><Del>', "<Esc>:execute 'silent !rm ' . expand('%') | bdelete<CR>")
+keymap.set('n', '<leader><Del>', "<Esc>:execute 'silent !rm ' . expand('%') | bdelete<CR>")
 
-vim.keymap.set({ "n", "v" }, "<A-d>", [["_d]])
-vim.keymap.set("n", "<A-x>", '"_x', opts)
+keymap.set({ "n", "v" }, "<A-d>", [["_d]])
+keymap.set("n", "<A-x>", '"_x', opts)
 
-vim.keymap.set({"n", "i"}, "<A-s>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+keymap.set({"n", "i"}, "<A-s>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
     { desc = "Replace word cursor is on globally" })
 
-vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
+keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
 
-vim.keymap.set("n", "<leader>.", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>.", "<C-w>v", { desc = "Split window vertically" })
 -- split window vertically
-vim.keymap.set("n", "<leader>,", "<C-w>s", { desc = "Split window horizontally" })
