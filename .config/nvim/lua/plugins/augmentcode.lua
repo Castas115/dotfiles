c06db08028e076f12407ca12dc92c1901b264628
jon <joncastas@gmail.com>
2025-05-07 13:00:12 +0200

    nvim: augment, debugger and grug-far added

---
+++ .config/nvim/lua/plugins/augmentcode.lua
@@ -0,0 +1,13 @@
+return {
+	"augmentcode/augment.vim",
+	config = function()
+		vim.g.augment_workspace_folders = {
+			"/home/j.castander/dotfiles/.config",
+			"/home/j.castander/salto_gh/pricebook.saltosystems.com",
+			"/home/j.castander/programming/goEcon/",
+		}
+
+		vim.keymap.set({"n", "v"}, "<leader>aa", "<cmd>Augment chat<cr>", { desc = "[A]ugment chat" })
+		vim.keymap.set({"n", "v"}, "<leader>at", "<cmd>Augment chat-toggle<cr>", { desc = "[A]ugment chat toggle" })
+	end,
+}
