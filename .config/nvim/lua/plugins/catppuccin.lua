7ee31278b1065e3304406c967263bba11fb31270
jon <joncastas@gmail.com>
2025-05-26 09:21:23 +0200

    all catpucchin is changed to pure black

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,44 @@
-return { 
-	"catppuccin/nvim", 
-	name = "catppuccin", 
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup()
-		vim.cmd.colorscheme "catppuccin"
-	end
+		vim.cmd.colorscheme("catppuccin")
+	end,
+	opts = {
+		no_italic = true,
+		term_colors = true,
+		transparent_background = false,
+		styles = {
+			comments = {},
+			conditionals = {},
+			loops = {},
+			functions = {},
+			keywords = {},
+			strings = {},
+			variables = {},
+			numbers = {},
+			booleans = {},
+			properties = {},
+			types = {},
+		},
+		color_overrides = {
+			mocha = {
+				base = "#000000",
+				mantle = "#000000",
+				crust = "#000000",
+			},
+		},
+		integrations = {
+			telescope = {
+				enabled = true,
+				style = "nvchad",
+			},
+			dropbar = {
+				enabled = true,
+				color_mode = true,
+			},
+		},
+	},
 }
