24f42ab4315a10c588627e2f546c8413170082dc
jon <joncastas@gmail.com>
2025-05-26 09:21:36 +0200

    nvim: hardtime

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+return {
+	"m4xshen/hardtime.nvim",
+	lazy = false,
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		disabled_keys = {
+			["<Down>"] = false,
+			["<Up>"] = false,
+			["<Rigt>"] = false,
+			["<Left>"] = false
+		},
+	},
+}
