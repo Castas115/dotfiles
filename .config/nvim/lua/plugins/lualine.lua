38d47173257a21a5b942b279772c6503f6876af2
jon <joncastas@gmail.com>
2025-05-28 11:57:21 +0200

    lualine change improved

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,50 +1,91 @@
 local function get_buffer_count()
-  local buffers = vim.fn.execute("ls")
-  local count = 0
-  -- Match only lines that represent buffers, typically starting with a number followed by a space
-  for line in string.gmatch(buffers, "[^\r\n]+") do
-    if string.match(line, "^%s*%d+") then
-      count = count + 1
-    end
-  end
-  return count
+	local buffers = vim.fn.execute("ls")
+	local count = 0
+	-- Match only lines that represent buffers, typically starting with a number followed by a space
+	for line in string.gmatch(buffers, "[^\r\n]+") do
+		if string.match(line, "^%s*%d+") then
+			count = count + 1
+		end
+	end
+	return count
 end
 
+local colors = {
+	color9 = "#3e445e",
+
+	normal = "#818596",
+	insert = "#84a0c6",
+	visual = "#b4be82",
+	replace = "#e2a478",
+
+	background = "#16171f",
+	black = "#000000",
+	text = "#c6c8d1",
+}
+
+local custom_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.normal, gui = "bold" },
+		b = { fg = colors.text, bg = colors.background },
+		c = { fg = colors.text, bg = colors.black },
+	},
+	insert = {
+		a = { fg = colors.black, bg = colors.insert, gui = "bold" },
+	},
+	visual = {
+		a = { fg = colors.black, bg = colors.visual, gui = "bold" },
+	},
+	replace = {
+		a = { fg = colors.black, bg = colors.replace, gui = "bold" },
+	},
+	inactive = {
+		a = { fg = colors.color9, bg = colors.black, gui = "bold" },
+		b = { fg = colors.color9, bg = colors.black },
+		c = { fg = colors.color9, bg = colors.black },
+	},
+}
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require('lualine').setup({
+		require("lualine").setup({
 			options = {
-				theme = 'iceberg_dark',
-				component_separators = '|',
-				section_separators = '',
+				theme = custom_theme,
+				component_separators = "",
+				section_separators = "",
+				globalstatus = true,
 			},
 			sections = {
-				lualine_a = {get_buffer_count},
-				lualine_b = { {
-					'filename',
-					file_status = true, -- Display modified/read-only status
-					path = 1,
-				}},
-				lualine_c = {},
-				-- lualine_c = {
-				-- 	{ require('NeoComposer.ui').status_recording },
-				-- },
+				lualine_a = { get_buffer_count },
+				lualine_b = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+					},
+				},
+				lualine_c = {
+					"searchcount",
+				},
 				lualine_x = {
+					{ "lsp_status" },
 					{
 						require("noice").api.status.command.get,
 						cond = require("noice").api.status.command.has,
 						color = { fg = "#ff9e64" },
 					},
 					{
-						'location',
+						"progress",
+						color = { fg = "#ff9e64" },
+					},
+					{
+						"location",
 						color = { fg = "#ff9e64" },
 					},
 				},
-				lualine_y = {'branch', 'diff', 'diagnostics'},
-				lualine_z = {'mode'}
-			}
+				lualine_y = { "diagnostics", "diff", "branch" },
+				lualine_z = { "mode" },
+			},
 		})
-	end
+	end,
 }
