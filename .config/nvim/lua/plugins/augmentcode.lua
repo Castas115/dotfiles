441de107a0d3867c66bceeaf522258a45abb8728
jon <joncastas@gmail.com>
2025-06-08 16:59:18 +0200

    arandr: update

--- .config/nvim/lua/plugins/augmentcode.lua
+++ .config/nvim/lua/plugins/augmentcode.lua
@@ -5,6 +5,7 @@ return {
 			"/home/j.castander/dotfiles/.config",
 			"/home/j.castander/salto_gh/pricebook.saltosystems.com",
 			"/home/j.castander/programming/goEcon/",
+			"/home/j.castander/programming/service-pattern-go/",
 		}
 
 		vim.keymap.set({"n", "v"}, "<leader>aa", "<cmd>Augment chat<cr>", { desc = "[A]ugment chat" })
