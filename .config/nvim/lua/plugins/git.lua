cdc9a1168f6569dada4e400431159657965e9544
jon <joncastas@gmail.com>
2025-06-17 11:49:27 +0200

    hardtime removed and gitsigns added to see blame
    a

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,47 +1,56 @@
 return {
 	{
-		'echasnovski/mini.diff',
-		version = '*',
+		"echasnovski/mini.diff",
+		version = "*",
 		config = function()
-			require('mini.diff').setup {
-				vim.keymap.set('n', "<leader>gd", ':lua MiniDiff.toggle_overlay()<CR>', { desc = "Git diff" }),
+			require("mini.diff").setup({
+				vim.keymap.set("n", "<leader>gD", ":lua MiniDiff.toggle_overlay()<CR>", { desc = "Git diff" }),
 				view = {
-					style = 'sign', -- 'number' or 'sign',
-					signs = { add = '┃', change = '┃', delete = '▁' },
+					style = "sign", -- 'number' or 'sign',
+					signs = { add = "┃", change = "┃", delete = "▁" },
 					priority = 199, -- Priority of used visualization extmarks
 				},
 
 				mappings = {
-					apply = 'gh',
-					reset = 'gH',
-					textobject = 'gh',
+					apply = "gh",
+					reset = "gH",
+					textobject = "gh",
 
-					goto_first = '[H',
-					goto_prev = '[h',
-					goto_next = ']h',
-					goto_last = ']H',
+					goto_first = "[H",
+					goto_prev = "[h",
+					goto_next = "]h",
+					goto_last = "]H",
 				},
-			}
-		end
-	},{
+			})
+		end,
+	},
+	{
 		"NeogitOrg/neogit",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"sindrets/diffview.nvim",
 			"nvim-telescope/telescope.nvim",
 		},
-		cmd = 'Neogit',
+		cmd = "Neogit",
 		keys = {
-			{ '<leader>gn', ':Neogit kind=floating<cr>', desc = 'neo[g]it' },
+			{ "<leader>gn", ":Neogit kind=floating<cr>", desc = "neo[g]it" },
 		},
 		config = function()
-			require('neogit').setup {
+			require("neogit").setup({
 				disable_commit_confirmation = true,
 				integrations = {
 					diffview = true,
 				},
-			}
+			})
 		end,
-	}
-}
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns diffthis<CR>", { desc = "Git Diff"})
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "Git Blame"})
+		end,
+	},
+}
--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,13 +1,14 @@
 return {
-	"m4xshen/hardtime.nvim",
-	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
-	opts = {
-		disabled_keys = {
-			["<Down>"] = false,
-			["<Up>"] = false,
-			["<Rigt>"] = false,
-			["<Left>"] = false
-		},
-	},
+	-- "m4xshen/hardtime.nvim",
+	-- lazy = false,
+	-- dependencies = { "MunifTanjim/nui.nvim" },
+	-- opts = {
+	-- 	
+	-- 	disabled_keys = {
+	-- 		["<Down>"] = false,
+	-- 		["<Up>"] = false,
+	-- 		["<Rigt>"] = false,
+	-- 		["<Left>"] = false
+	-- 	},
+	-- },
 }
