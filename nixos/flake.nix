a627a4af9595f4f38594fdeb7d33a591f348e4c9
Jon <joncastas@gmail.com>
2025-11-13 07:31:09 +0100

    nixos on dotfiles

---
+++ nixos/flake.nix
@@ -0,0 +1,30 @@
+{
+	description = "My NixOS";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		home-manager.url = "github:nix-community/home-manager/release-25.05";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.jon = import ./home.nix;
+						backupFileExtension = "backup";
+					};
+				}
+			];
+		};
+	};
+}
---
+++ nixos/home.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "jon";
+	home.homeDirectory = "/home/jon";
+
+	programs.git.enable = true;
+	home.stateVersion = "25.05";
+
+	programs.atuin = {
+		enable = true;
+		enableBashIntegration = true;
+		enableFishIntegration = true;
+		enableNushellIntegration = true;
+	};
+
+	programs.zoxide = {
+		enable = true;
+		enableBashIntegration = true;
+		enableFishIntegration = true;
+		enableNushellIntegration = true;
+	};
+
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			btw = "echo I use nixos, btw";
+			ll = "ls -alF";
+			la = "ls -A";
+			l  = "ls -CF";
+			ai = "sudo apt-get install -y ";
+			c  = "__zoxide_z";
+			cf = "__zoxide_zi";
+			v  = "nvim";
+			g  = "lazygit";
+
+		};
+	};
+
+	home.file./
+
+	home.packages = with pkgs; [
+		neovim 
+		vivaldi
+		tmux
+		fish
+		nushell
+		copyq
+		zoxide
+		atuin
+		sshfs
+		ghostty
+		kitty
+		stow
+		lazygit
+		hyprlock
+		hypridle
+		nwg-look
+		waybar
+		wofi
+		delta
+		keepassxc
+		blueberry
+		kanshi
+		jq
+		hyprshot
+		swaynotificationcenter
+		vlc
+		bibata-cursors
+	];
+
+}
