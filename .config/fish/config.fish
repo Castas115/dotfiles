38e83c9db48859d0994e8073397d132bd8b1dead
jcastander <jcastander@swapenergia.com>
2025-11-13 10:51:43 +0100

    deleted fish shit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,16 +35,3 @@ abbr t  'tmux'
 abbr ta  'tmux attach'
 abbr tl  'tmux ls'
 
-
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-
-
-# salto things
-abbr po 'bash ~/programming/scripts/pricebook-options.sh'
