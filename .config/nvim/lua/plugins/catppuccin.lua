1da3a33ddf35e04838eb2f4ea60afb0d0e9dbef5
Jon <joncastas@gmail.com>
2025-12-08 12:24:48 +0100

    nvim: catpuccin background

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,8 +2,8 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	config = function()
-		require("catppuccin").setup()
+	config = function(_,opts)
+		require("catppuccin").setup(opts)
 		vim.cmd.colorscheme("catppuccin")
 	end,
 	opts = {
