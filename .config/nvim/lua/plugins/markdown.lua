2197952ec5bcb71d4aba5f8e657c09948d53ffb5
Jon <joncastas@gmail.com>
2025-12-19 09:21:04 +0100

    nvim: custom stopped checkbox

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,14 @@ return {
 	{
 		"MeanderingProgrammer/markdown.nvim",
 		main = "render-markdown",
-		opts = {},
+		opts = {
+			checkbox = {
+				custom = {
+													--      󰏧
+					stopped = { raw = '[/]', rendered = '', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+				},
+			},
+		},
 		name = "render-markdown",                                                      -- Only needed if you have another plugin named markdown.nvim
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you use the mini.nvim suitable
 	},
