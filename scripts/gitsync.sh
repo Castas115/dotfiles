4caf18354cc7ec0ae0ce5a4773b47339d7cc4b7d
Jon <joncastas@gmail.com>
2025-12-22 19:32:08 +0100

    squash incomplete not applied yet

--- scripts/gitsync.sh
+++ scripts/gitsync.sh
@@ -17,42 +17,42 @@ squash_hourly_commits() {
   fi
 }
 
-squash_incomplete_hours() {
-  local path="$1"
-  local today=$(date +"%Y-%m-%d")
-  
-  cd "$path"
-  
-  # Get all commits from today
-  local commits_today=$(git log --since="$today 00:00:00" --format="%H %ci" | head -50)
-  
-  if [[ -z "$commits_today" ]]; then
-    return
-  fi
-  
-  # Get the latest commit timestamp
-  local latest_commit=$(echo "$commits_today" | head -n 1)
-  local latest_time=$(echo "$latest_commit" | cut -d' ' -f2)
-  local latest_hour=$(echo "$latest_time" | cut -d':' -f1)
-  local latest_minute=$(echo "$latest_time" | cut -d':' -f2)
-  
-  # If latest commit is not at x:00:00, squash the hour
-  if [[ "$latest_minute" != "00" ]]; then
-    local hour_start="$today $latest_hour:00:00"
-    local hour_end="$today $latest_hour:59:59"
-    
-    # Get commits in this incomplete hour
-    local hour_commits=$(git log --since="$hour_start" --until="$hour_end" --oneline | wc -l)
-    
-    if [[ $hour_commits -gt 1 ]]; then
-      echo "Squashing incomplete hour $latest_hour in $path"
-      local base_commit=$(git log --since="$hour_start" --format="%H" | tail -n 1)
-      git reset --soft "$base_commit"^
-      git commit -m "Hourly squash: $today $latest_hour:00"
-      git push --force-with-lease
-    fi
-  fi
-}
+# squash_incomplete_hours() {
+#   local path="$1"
+#   local today=$(date +"%Y-%m-%d")
+#   
+#   cd "$path"
+#   
+#   # Get all commits from today
+#   local commits_today=$(git log --since="$today 00:00:00" --format="%H %ci" | head -50)
+#   
+#   if [[ -z "$commits_today" ]]; then
+#     return
+#   fi
+#   
+#   # Get the latest commit timestamp
+#   local latest_commit=$(echo "$commits_today" | head -n 1)
+#   local latest_time=$(echo "$latest_commit" | cut -d' ' -f2)
+#   local latest_hour=$(echo "$latest_time" | cut -d':' -f1)
+#   local latest_minute=$(echo "$latest_time" | cut -d':' -f2)
+#   
+#   # If latest commit is not at x:00:00, squash the hour
+#   if [[ "$latest_minute" != "00" ]]; then
+#     local hour_start="$today $latest_hour:00:00"
+#     local hour_end="$today $latest_hour:59:59"
+#     
+#     # Get commits in this incomplete hour
+#     local hour_commits=$(git log --since="$hour_start" --until="$hour_end" --oneline | wc -l)
+#     
+#     if [[ $hour_commits -gt 1 ]]; then
+#       echo "Squashing incomplete hour $latest_hour in $path"
+#       local base_commit=$(git log --since="$hour_start" --format="%H" | tail -n 1)
+#       git reset --soft "$base_commit"^
+#       git commit -m "Hourly squash: $today $latest_hour:00"
+#       git push --force-with-lease
+#     fi
+#   fi
+# }
 
 auto_git_push() {
   local path="$1"
