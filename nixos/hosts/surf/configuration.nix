460d5f6ec8206d64d8743d8f9e42e6ac7f4c3885
Jon <joncastas@gmail.com>
2026-02-09 00:24:57 +0100

    nix: hibernation on surface

--- nixos/hosts/surf/configuration.nix
+++ nixos/hosts/surf/configuration.nix
@@ -5,4 +5,17 @@
     [
       ./hardware-configuration.nix
     ];
+
+  # Enable hibernation (resume from swap)
+  boot.resumeDevice = "/dev/disk/by-uuid/8e4e0e2d-c59e-4aee-b830-5be8de4b7902";
+
+  # Minimal hibernation image for faster resume
+  systemd.tmpfiles.rules = [
+    "w /sys/power/image_size - - - - 0"
+  ];
+
+  # Suspend-then-hibernate: suspend first, hibernate after 30 min
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30min
+  '';
 }
