0c6db9f03f0c3865378e9bdda1c535614798ea32
Jon <joncastas@gmail.com>
2026-02-28 10:20:20 +0100

    nvim: many fixes
    a

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -1,89 +1,85 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		{
-			"williamboman/mason.nvim",
-			build = function()
-				pcall(vim.cmd, "MasonUpdate")
-			end,
-		},
-		{ "williamboman/mason-lspconfig.nvim" },
+		{ "williamboman/mason.nvim" },  -- UI for LSP management
 		{ "hrsh7th/nvim-cmp" },
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "L3MON4D3/LuaSnip" },
 		{ "saadparwaiz1/cmp_luasnip" },
 	},
 	config = function()
-		-- First, let's verify lspconfig is working
-		local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
-		if not lspconfig_ok then
-			vim.notify("LSPConfig failed to load!", vim.log.levels.ERROR)
-			return
-		end
-
-		-- Setup Mason first
+		-- Setup Mason (for UI only, LSPs installed via Nix)
 		require("mason").setup()
 
-		-- Wait a bit and then setup mason-lspconfig
-		vim.defer_fn(function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "lua_ls", "rust_analyzer" },
-			})
-		end, 100)
-
-		-- Basic capabilities
+		-- Capabilities for autocompletion
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		-- Key mappings
-		local on_attach = function(client, bufnr)
-			local set_keymap = function(mode, keymap, func, desc)
-				vim.keymap.set(mode, keymap, func, { buffer = bufnr, remap = false, desc = desc })
-			end
+		-- Key mappings on LSP attach
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local bufnr = args.buf
+				local set_keymap = function(mode, keymap, func, desc)
+					vim.keymap.set(mode, keymap, func, { buffer = bufnr, remap = false, desc = desc })
+				end
 
-			set_keymap("n", "K", vim.lsp.buf.hover, "LSP Hover")
-			set_keymap("n", "<leader>ld", vim.lsp.buf.definition, "Definition")
-			set_keymap("n", "<leader>lr", vim.lsp.buf.references, "References")
-			set_keymap("n", "<leader>la", vim.lsp.buf.code_action, "Code Action")
-			set_keymap("n", "<leader>ln", vim.lsp.buf.rename, "Rename")
-			set_keymap("n", "L", vim.diagnostic.setloclist, "Show Diagnostics")
-			set_keymap("n", "<leader>ls", vim.lsp.buf.workspace_symbol, "Workspace Symbol")
-			set_keymap("i", "<A-h>", vim.lsp.buf.signature_help, "Signature Help")
-			set_keymap("n", "<leader>li", function ()
-				vim.lsp.buf.code_action({
-					apply = true,
-					context = { diagnostics = {}, only = { "source.organizeImports" } },
-				})
-			end, "Organize Imports")
-		end
+				set_keymap("n", "K", vim.lsp.buf.hover, "LSP Hover")
+				set_keymap("n", "<leader>ld", vim.lsp.buf.definition, "Definition")
+				set_keymap("n", "<leader>lr", vim.lsp.buf.references, "References")
+				set_keymap("n", "<leader>la", vim.lsp.buf.code_action, "Code Action")
+				set_keymap("n", "<leader>ln", vim.lsp.buf.rename, "Rename")
+				set_keymap("n", "L", vim.diagnostic.setloclist, "Show Diagnostics")
+				set_keymap("n", "<leader>ls", vim.lsp.buf.workspace_symbol, "Workspace Symbol")
+				set_keymap("i", "<A-h>", vim.lsp.buf.signature_help, "Signature Help")
+				set_keymap("n", "<leader>li", function()
+					vim.lsp.buf.code_action({
+						apply = true,
+						context = { diagnostics = {}, only = { "source.organizeImports" } },
+					})
+				end, "Organize Imports")
+			end,
+		})
 
-		-- Manual server setup (bypass mason-lspconfig for now)
+		-- Neovim 0.11+ uses vim.lsp.config
 		local servers = {
-			gopls = {},
-			lua_ls = {
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-						telemetry = { enable = false },
-					},
-				},
-			},
-			rust_analyzer = {},
+			"gopls",
+			"rust_analyzer",
+			"bashls",
+			"pyright",
+			"ts_ls",
+			"html",
+			"cssls",
+			"jsonls",
+			"yamlls",
+			"dockerls",
+			"terraformls",
+			"taplo",
+			"marksman",
+			"nil_ls",
+			"phpactor",
 		}
 
-		-- Setup servers manually
-		for server, config in pairs(servers) do
-			config.on_attach = on_attach
-			config.capabilities = capabilities
-
-			-- Try to setup each server
-			local setup_ok, _ = pcall(lspconfig[server].setup, config)
-			if not setup_ok then
-				vim.notify("Failed to setup " .. server, vim.log.levels.WARN)
-			end
+		-- Configure all servers with default capabilities
+		for _, server in ipairs(servers) do
+			vim.lsp.config(server, { capabilities = capabilities })
 		end
 
-		-- Basic completion setup
+		-- Lua needs extra config
+		vim.lsp.config("lua_ls", {
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+					workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+					telemetry = { enable = false },
+				},
+			},
+		})
+
+		-- Enable all servers
+		table.insert(servers, "lua_ls")
+		vim.lsp.enable(servers)
+
+		-- Completion setup
 		local cmp = require("cmp")
 		cmp.setup({
 			snippet = {
@@ -102,11 +98,5 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
 		})
-
-		-- Debug info
-		vim.api.nvim_create_user_command("LspDebug", function()
-			print("Available servers:", vim.inspect(vim.tbl_keys(lspconfig)))
-			print("Active clients:", vim.inspect(vim.lsp.get_active_clients()))
-		end, {})
 	end,
 }
