0423337d82863b3da8b047b04131323e106c5e3f
Jon <joncastas@gmail.com>
2026-03-14 15:39:52 +0100

    nixos: refactor

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,53 +1,37 @@
 {
-	description = "My NixOS";
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-		home-manager.url = "github:nix-community/home-manager/release-25.05";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		claude-code-nix.url = "github:sadjow/claude-code-nix";
-		wiremix.url = "github:tsowell/wiremix";
-	};
+  description = "My NixOS";
 
-	
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    claude-code-nix.url = "github:sadjow/claude-code-nix";
+    wiremix.url = "github:tsowell/wiremix";
+  };
+
   outputs = { self, nixpkgs, home-manager, claude-code-nix, wiremix, ... }:
     let
       system = "x86_64-linux";
+
+      mkHost = hostname: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/${hostname}/hardware-configuration.nix
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.jon = import ./home.nix;
+              backupFileExtension = "backup";
+              extraSpecialArgs = { inherit claude-code-nix wiremix; };
+            };
+          }
+        ];
+      };
     in {
-
-      nixosConfigurations.work = nixpkgs.lib.nixosSystem {
-        inherit system;
-			modules = [
-				./hosts/work/hardware-configuration.nix
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.jon = import ./home.nix;
-						backupFileExtension = "backup";
-						extraSpecialArgs = { inherit claude-code-nix wiremix; };
-					};
-				}
-			];
-		};
-
-      nixosConfigurations.surf = nixpkgs.lib.nixosSystem {
-        inherit system;
-			modules = [
-				./hosts/surf/hardware-configuration.nix
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.jon = import ./home.nix;
-						backupFileExtension = "backup";
-						extraSpecialArgs = { inherit claude-code-nix wiremix; };
-					};
-				}
-			];
-		};
-	};
+      nixosConfigurations.work = mkHost "work";
+      nixosConfigurations.surf = mkHost "surf";
+    };
 }
