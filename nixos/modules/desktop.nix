217a91bdc76c1ac2c9d13542b532ae7243df586d
Jon <joncastas@gmail.com>
2026-03-31 23:10:18 +0200

    nixos: bluetooth reconnect added

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -45,6 +45,19 @@
   services.printing.enable = true;
   services.flatpak.enable = true;
   hardware.keyboard.zsa.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+        ReconnectAttempts = 7;
+        ReconnectIntervals = "1,2,4,8,16,32,64";
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     nerd-fonts.jetbrains-mono
