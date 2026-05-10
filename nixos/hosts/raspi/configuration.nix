e77613f414a204a4af47ff680413a718fe0c7f17
Jon <joncastas@gmail.com>
2026-05-10 18:14:04 +0200

    nixos: raspi fish

--- nixos/hosts/raspi/configuration.nix
+++ nixos/hosts/raspi/configuration.nix
@@ -1,5 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.openssh.enable = true;
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      functions -q __orig_fish_prompt; or functions --copy fish_prompt __orig_fish_prompt
+      function fish_prompt
+          set_color red --bold
+          echo -n 'pi '
+          set_color normal
+          __orig_fish_prompt
+      end
+    '';
+  };
+  users.users.jon.shell = pkgs.fish;
 }
