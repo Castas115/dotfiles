d09ec911fef14a19f797214f44df18218cb0f5a4
Jon <joncastas@gmail.com>
2026-06-10 16:03:23 +0200

    nvim: grug far toggle and quit

--- .config/nvim/lua/plugins/grug-far.lua
+++ .config/nvim/lua/plugins/grug-far.lua
@@ -8,15 +8,26 @@ return {
 			function()
 				local grug = require("grug-far")
 				local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-				grug.open({
-					transient = true,
+				grug.toggle_instance({
+					instanceName = "main",
+					staticTitle = "Find and Replace",
 					prefills = {
 						filesFilter = ext and ext ~= "" and "*." .. ext or nil,
 					},
 				})
 			end,
 			mode = { "n", "v" },
-			desc = "Search and Replace",
+			desc = "Search and Replace (toggle)",
 		},
 	},
+	config = function(_, opts)
+		require("grug-far").setup(opts)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "grug-far",
+			callback = function(args)
+				local map_opts = { buffer = args.buf, silent = true, desc = "Hide grug-far" }
+				vim.keymap.set("n", "q", "<cmd>close<CR>", map_opts)
+			end,
+		})
+	end,
 }
